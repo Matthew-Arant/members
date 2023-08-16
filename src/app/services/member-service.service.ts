1b9ac7a95c026ec9cd77afe087d3cc5f15350a97
Matthew Arant <rex.legorium@gmail.com>
2023-08-15 18:58:54 -0700

    updated getURL

--- src/app/services/member-service.service.ts
+++ src/app/services/member-service.service.ts
@@ -31,7 +31,7 @@ export class MemberService {
   }
 
   private getURL(): string {
-    return 'http://localhost:3000/member'
+    return 'membersapi.vercel.app/member'
   }
 
   private getURLWithId(id: number): string {
